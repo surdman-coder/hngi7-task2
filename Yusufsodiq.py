80efd91656a904d50bd4d6f44ee39bd4339c2ce1
surdman-coder <66185908+surdman-coder@users.noreply.github.com>
2020-06-04 18:02:54 +0100

    Create Yusufsodiq.py

---
+++ Yusufsodiq.py
@@ -0,0 +1,6 @@
+myname= 'Yusuf Sodiq'
+myID= 'HNG-02857'
+myemail= 'yusufsodiq22@gmail.com'
+Lang= 'python'
+
+print('Hello world, this is '+my_name+' with HNGi7 ID '+my_ID+' using '+Lang+' for stage 2 task')
